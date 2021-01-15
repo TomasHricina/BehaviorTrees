bcc6ae13aa25cb3b870b930b7d8da1dbc2cdb6ad
TomasHricina <77452604+TomasHricina@users.noreply.github.com>
2021-01-15 15:10:06 +0100

    Update t01_build_your_first_tree.cpp

--- t01_build_your_first_tree.cpp
+++ t01_build_your_first_tree.cpp
@@ -1,13 +1,33 @@
-
 #include "behaviortree_cpp_v3/bt_factory.h"
 #include "dummy_nodes.h"
+
+class ApproachObject : public BT::SyncActionNode
+{
+  public:
+    ApproachObject(const std::string& name) :
+        BT::SyncActionNode(name, {})
+    {
+    }
+
+    BT::NodeStatus tick() override
+    {
+        std::cout << "ApproachObject: " << this->name() << std::endl;
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
 
 int main()
 {
     BehaviorTreeFactory factory;
+
     using namespace DummyNodes;
+
     factory.registerNodeType<ApproachObject>("ApproachObject");
+
     auto tree = factory.createTreeFromFile("./my_tree.xml");
+
     tree.tickRoot();
+
     return 0;
 }
